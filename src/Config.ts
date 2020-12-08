355d427c41c5f4d524c6c52658aaab6e3cffc735
antisynth <73140189+antisynth@users.noreply.github.com>
2020-12-07 23:42:06 -0700

    Remove config

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
-export let token: string = 'XD';
+export let token: string = 'NzU1MTUzMTk5NjEwMjAwMTE2.X1_JOA.Ysmi9oJnbOwSuL3X4tGBUabAPzo';
 export let prefix: string = "!";
 export let owners: string[] = ['461340349680582667'];
