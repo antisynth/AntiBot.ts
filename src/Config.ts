562dc9651b8e82d28b1f72f1b9b97d09650e1030
antisynth <73140189+antisynth@users.noreply.github.com>
2020-12-07 23:37:30 -0700

    e

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
-export let token: string = 'NzU1MTUzMTk5NjEwMjAwMTE2.X1_JOA.P2dRQ1QShq5Wi8ZkOjUh-OEUm7s';
+export let token: string = 'TOKEN';
 export let prefix: string = "!";
 export let owners: string[] = ['461340349680582667'];
