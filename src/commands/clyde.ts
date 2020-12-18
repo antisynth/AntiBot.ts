116fee60239266a80c23ad7473089f113325433d
antisynth <73140189+antisynth@users.noreply.github.com>
2020-12-17 20:11:59 -0700

    let clyde work with pings

--- src/commands/clyde.ts
+++ src/commands/clyde.ts
@@ -27,6 +27,24 @@ export default class Clyde extends Command {
 
     public exec(message: Message, { text }: { text: string }) {
         if (!text) return message.util.send('Please provide some text')
+        
+        const regex = new RegExp(/<@!*\d{18}>/g)
+        if (regex.test(text)) {
+            const newtext = text.replace(/<@!*/g, '')
+            const newnewtext = newtext.replace(/>/g, '')
+            const newnewnewtext = newnewtext.replace(
+                /[^\d{18}]/g, ''
+            )
+            let userinputted = message.guild.members.cache.get(newnewnewtext)
+
+            let userinputtednick = userinputted.nickname
+            if (userinputtednick == null) {
+                userinputtednick = userinputted.user.username
+            }
+
+            text = text.replace(/<@!*\d{18}>/g, userinputtednick)
+        }
+
         axios
         .get(`https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`)
         .then((res) => {
