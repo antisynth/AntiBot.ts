1c2bcc10787b8ea501eb5800e4a67670a308594e
antisynth <73140189+antisynth@users.noreply.github.com>
2020-12-18 11:42:00 -0700

    Update Bot.ts

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,6 +1,5 @@
-import { token, owners, prefix } from './Config';
+import { token, owners } from './Config';
 import BotClient from "./client/BotClient";
-import { Message } from 'discord.js'
 
 export const client: BotClient = new BotClient({ token, owners });
 console.clear();
