77b3d998f6d05aaa1cf934104eacaaf77c09a1ca
antisynth <73140189+antisynth@users.noreply.github.com>
2020-12-17 23:19:39 -0700

    clyde error handling

--- src/commands/clyde.ts
+++ src/commands/clyde.ts
@@ -26,35 +26,44 @@ export default class Clyde extends Command {
     }
 
     public exec(message: Message, { text }: { text: string }) {
-        if (!text) return message.util.send('Please provide some text')
-        
-        const regex = new RegExp(/<@!*\d{18}>/g)
-        if (regex.test(text)) {
-            const newtext = text.replace(/<@!*/g, '')
-            const newnewtext = newtext.replace(/>/g, '')
-            const newnewnewtext = newnewtext.replace(
-                /[^\d{18}]/g, ''
-            )
-            let userinputted = message.guild.members.cache.get(newnewnewtext)
+        try {
+            if (!text) return message.util.send('Please provide some text')
 
-            let userinputtednick = userinputted.nickname
-            if (userinputtednick == null) {
-                userinputtednick = userinputted.user.username
+            const regex = new RegExp(/<@!*\d{18}>/g)
+            if (regex.test(text)) {
+                const newtext = text.replace(/<@!*/g, '')
+                const newnewtext = newtext.replace(/>/g, '')
+                const newnewnewtext = newnewtext.replace(
+                    /[^\d{18}]/g, ''
+                )
+                let userinputted = message.guild.members.cache.get(newnewnewtext)
+
+                let userinputtednick = userinputted.nickname
+                if (userinputtednick == null) {
+                    userinputtednick = userinputted.user.username
+                }
+
+                text = text.replace(/<@!*\d{18}>/g, userinputtednick)
             }
 
-            text = text.replace(/<@!*\d{18}>/g, userinputtednick)
-        }
-
-        axios
-        .get(`https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`)
-        .then((res) => {
-            message.util.send('', {
-                files: [
-                    {
-                        attachment: res.data.message
-                    }
-                ]
+            axios
+            .get(`https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`)
+            .then((res) => {
+                message.util.send('', {
+                    files: [
+                        {
+                            attachment: res.data.message
+                        }
+                    ]
+                })
             })
-        })
+            .catch(
+                () => message.util.send(`TypeError [ERR_UNESCAPED_CHARACTERS]: You can't have emojis or hashtags in your text!`)
+            )
+        } catch {
+            return message.util.send(
+                'You can\'t have emojis or hashtags in your text!'
+            )
+        }
     }
 }
