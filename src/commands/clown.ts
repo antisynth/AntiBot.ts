d0f7f1ab73414829d8daecced4cc75fd1c7a0bdd
antisynth <73140189+antisynth@users.noreply.github.com>
2021-09-17 12:50:32 -0600

    update clown

--- src/commands/clown.ts
+++ src/commands/clown.ts
@@ -24,36 +24,66 @@ export default class Clown extends Command {
                     type: 'member',
                     match: 'rest',
                     default: (msg: Message) => msg.member
+                },
+                {
+                    id: 'tricky',
+                    type: 'string',
+                    match: 'option',
+                    flag: '-TRICKY=',
+                    default: 'FALSE'
                 }
             ]
         })
     }
 
-    public async exec(message: Message, { user }: { user: GuildMember }): Promise<Message> {
-        const canvas = Canvas.createCanvas(1206, 660)
-        const ctx = canvas.getContext('2d')
+    public async exec(message: Message, { user, tricky }: { user: GuildMember, tricky: string }): Promise<Message> {
+        if (tricky.toUpperCase() === 'TRUE') {
+            const canvas = Canvas.createCanvas(1206, 660)
+            const ctx = canvas.getContext('2d')
 
-        const background = await Canvas.loadImage(
-            path.join(__dirname, '../files/omfgclown.jpg')
-        )
+            const background = await Canvas.loadImage(
+                path.join(__dirname, '../files/omfgclown.jpg')
+            )
 
-        const avatar = await Canvas.loadImage(
-            user.user.displayAvatarURL({ format: 'jpg' })
-        )
+            const avatar = await Canvas.loadImage(
+                user.user.displayAvatarURL({ format: 'jpg' })
+            )
 
-        const foreground = await Canvas.loadImage(
-            path.join(__dirname, '../files/omfgclownforeground.png')
-        )
+            const foreground = await Canvas.loadImage(
+                path.join(__dirname, '../files/omfgclownforeground.png')
+            )
 
-        let x = 0
-        let y = 0
+            let x = 0
+            let y = 0
 
-        ctx.drawImage(background, x, y)
-        ctx.drawImage(avatar, 250, 250, 300, 300)
-        ctx.drawImage(foreground, x, y)
+            ctx.drawImage(background, x, y)
+            ctx.drawImage(avatar, 250, 250, 300, 300)
+            ctx.drawImage(foreground, x, y)
 
-        const attachment = new MessageAttachment(canvas.toBuffer(), 'clown.png')
+            const attachment = new MessageAttachment(canvas.toBuffer(), 'clown.png')
 
-        return message.util.send({ files: [attachment] })
+            return message.util.send({ files: [attachment] })
+        } else {
+            const canvas = Canvas.createCanvas(683, 1024)
+            const ctx = canvas.getContext('2d')
+
+            const background = await Canvas.loadImage(
+                path.join(__dirname, '../files/clown.jpg')
+            )
+
+            const avatar = await Canvas.loadImage(
+                user.user.displayAvatarURL({ format: 'jpg' })
+            )
+
+            let x = 0
+            let y = 0
+
+            ctx.drawImage(background, x, y)
+            ctx.drawImage(avatar, 280, 280, 250, 250)
+
+            const attachment = new MessageAttachment(canvas.toBuffer(), 'clown.png')
+            
+            return message.util.send({ files: [attachment] })
+        }
     }
 }
